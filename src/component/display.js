126625643000727c85f4d7e33f7e204c1696c8da
Nandu064 <harish861432@gmail.com>
2021-07-03 15:07:51 +0530

    commited with minor changes

--- src/component/display.js
+++ src/component/display.js
@@ -8,16 +8,16 @@ function display({playerList,upadateHandler,deleteEmpHandler}) {
                 <Card style={{ width: '19rem',backgroundColor:'lightgray',height:'auto' }} >
                 <Card.Body>
                   <Card.Text style={{display:'flex',flexDirection:'column',justifyContent:'space-evenly'}}>
-                    <span style={{color:'black'}}>Id: {item.no}  </span>
-                    <span style={{color:'black'}}>Name: {item.name}  </span>
-                    <span style={{color:'black'}}>Age : {item.age} </span>
-                    <span style={{color:'black'}}>Current Club : {item.currentclub} </span>
-                    <span style={{color:'black'}}>Position : {item.position} </span>
-                    <span style={{color:'black'}}>Debut Year : {item.debutyear} </span>
-                    <span style={{color:'black'}}>Previous Club : {item.previousclub} </span>
-                    <span style={{color:'black'}}>Goals : {item.goals} </span>
-                    <span style={{color:'black'}}>Assists : {item.assists} </span>
-                    <span style={{color:'black'}}>Free Kicks Scored : {item.freekickscored} </span>
+                    <span style={{color:'black',lineHeight:'30px'}}>Id: {item.no}  </span>
+                    <span style={{color:'black',lineHeight:'30px'}}>Name: {item.name}  </span>
+                    <span style={{color:'black',lineHeight:'30px'}}>Age : {item.age} </span>
+                    <span style={{color:'black',lineHeight:'30px'}}>Current Club : {item.currentclub} </span>
+                    <span style={{color:'black',lineHeight:'30px'}}>Position : {item.position} </span>
+                    <span style={{color:'black',lineHeight:'30px'}}>Debut Year : {item.debutyear} </span>
+                    <span style={{color:'black',lineHeight:'30px'}}>Previous Club : {item.previousclub} </span>
+                    <span style={{color:'black',lineHeight:'30px'}}>Goals : {item.goals} </span>
+                    <span style={{color:'black',lineHeight:'30px'}}>Assists : {item.assists} </span>
+                    <span style={{color:'black',lineHeight:'30px'}}>Free Kicks Scored : {item.freekickscored} </span>
                   </Card.Text>
                   <Button variant="secondary" onClick={()=>upadateHandler(item,index)} style={{margin:'10px'}}>Edit</Button>
                   <Button variant="danger" onClick={()=>deleteEmpHandler(index)}  >Delete</Button>
